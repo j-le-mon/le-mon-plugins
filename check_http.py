3d8e7ec8320b5c48e6abd07211965d767150db3c
Lukasz <lukasz.wieczorek@softnet.com.pl>
2019-12-04 16:35:43 +0100

    commit plugin http

--- check_http.py
+++ check_http.py
@@ -1,6 +1,6 @@
 from urllib2 import Request, urlopen
 from urllib2 import URLError, HTTPError
-req = Request('http://192.168.8.246')
+req = Request('http://ip_address')
 try:
     response = urlopen(req)
 except URLError as e:
@@ -11,6 +11,6 @@ except URLError as e:
         print ('Serwer nie moze wykonac zapytania', e.reason)
         raise SystemExit, 2
 else:
-    print ('OK - dziala polaczenie do KIR', response.getcode())
+    print ('OK - dziala polaczenie do ___usluga___', response.getcode())
     raise SystemExit, 0
 
